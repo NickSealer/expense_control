1f8e00476dcd8bec2783e5c143c784d03b22aa64
NickSealer <nickfontechav@gmail.com>
2024-03-16 17:49:51 -0500

    U. 22 update

--- app/queries/categories_query.rb
+++ app/queries/categories_query.rb
@@ -11,6 +11,6 @@ class CategoriesQuery
 
   def search(current_user, query)
     @categories.where('name ILIKE ?', "%#{query}%").where(user_id: current_user.id)
-               .or(@categories.where('name ILIKE ?', "%#{query}%").where(user_id: current_user.id))
+               .or(@categories.where('description ILIKE ?', "%#{query}%").where(user_id: current_user.id))
   end
 end
