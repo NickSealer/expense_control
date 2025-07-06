315caae1b9382a0998f9a5512fcce11cef78373f
NickSealer <nickfontechav@gmail.com>
2025-07-06 18:12:49 -0500

    minor changes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,7 +4,7 @@ module Users
   class RegistrationsController < Devise::RegistrationsController
     invisible_captcha only: [:create]
     # before_action :configure_sign_up_params, only: [:create]
-    # before_action :configure_account_update_params, only: [:update]
+    before_action :configure_account_update_params, only: [:update]
 
     def create
       super do |resource|
